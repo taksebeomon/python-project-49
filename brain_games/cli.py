be5eac357c9e47fa488cc993c911489e4a660d8c
taksebeomon <just.omon@yandex.com>
2024-01-14 21:40:30 +0500

    Adding welcome_user

---
+++ brain_games/cli.py
@@ -0,0 +1,3 @@
+import prompt
+
+name = prompt.string('May I have your name? ')
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -8,3 +8,11 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+from brain_games.cli import welcome_user
+
+def main():
+    welcome_user()
+
+if __name__ == "__main__":
+    main()
