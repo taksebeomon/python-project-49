d60d62857b33c0d49ebe4629ef6a28cb602ddc5c
taksebeomon <just.omon@yandex.com>
2024-01-23 17:56:39 +0500

    fixed errors

--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -17,8 +17,8 @@ def generate_round():
     question = str(number)
     correct_answer = 'yes' if is_prime(number) else 'no'
     instruction = (
-        "Answer 'yes' if given number is prime. "
-        "Otherwise answer 'no'."
+        'Answer "yes" if given number is prime. '
+        'Otherwise answer "no".'
     )
 
     return question, correct_answer, instruction
