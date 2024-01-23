c4031ae9ba3a5073f88fda02a624e3d528fe3f77
taksebeomon <just.omon@yandex.com>
2024-01-23 16:15:24 +0500

    fixed errors

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -4,7 +4,7 @@ import prompt
 
 def main():
     print("Welcome to the Brain Games!")
-    name = prompt.string('May I have your name, dorogusha? ')
+    name = prompt.string('May I have your name? ')
     print(f"Hello, {name}!")
     print('Answer "yes" if the number is even, otherwise answer "no".')
 
@@ -16,16 +16,13 @@ def main():
 
         if user_answer != correct_answer:
             print(
-                f"Sorry, but '{user_answer}' is wrong answer, dorogusha. "
+                f"Sorry, but '{user_answer}' is wrong answer. "
                 f"Correct answer was '{correct_answer}'"
             )
-            print(
-                f"Don't be upset, dorogusha! "
-                f"Start the game and let's try again, {name}!"
-            )
+            print(f"Start the game and let's try again, {name}!")
             return
         else:
-            print('Correct, dorogusha!')
+            print('Correct!')
 
     print(f"Congratulations, {name}")
 
